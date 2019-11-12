2929fc30d678f31ec78a569185825409108ea3a7
rajfruzsi <34722134+rajfruzsi@users.noreply.github.com>
2019-11-12 14:14:28 +0100

    cleaned code
    took out adding to db as separate function, calling it in others

--- app/Http/Controllers/UploaddrawingController.php
+++ app/Http/Controllers/UploaddrawingController.php
@@ -15,16 +15,11 @@ class UploaddrawingController extends Controller
 
 	function upload(Request $request){
 		if($request->file('rajz')->getClientOriginalExtension() != "png") return back()->with('error', 'Helytelen formátum!');
-
+		$ownname=$request->post('name');
 		$date = date('Y.m.d-H:i:s');
 		$filename = Auth::user()->id . '_' . $request->get('name') .'_' .  date('Y.m.d-H:i:s') . '.png';
 		$request->file('rajz')->storeAs(Auth::user()->id.'_'.Auth::user()->name, $filename);
-		DB::table('drawings')->insert([
-						'id' => Auth::user()->id,
-						'drawingname' => $request->post('name'),
-						'drawing_file_name' => $filename,
-						'drawing_created_at' => DB::raw('now()')
-					]);
+		self::addToDB($filename, $ownname);
 		return view('drawfigure');
 	}
 
@@ -37,12 +32,16 @@ class UploaddrawingController extends Controller
 		$img = base64_decode(explode(',', $data)[1]);
 		$filename = $upload_dir . time() . $ownname . '.png';
 		file_put_contents($filename, $img);
-		DB::table('drawings')->insert([
-						'id' => Auth::user()->id,
-						'drawingname' => $request->post('name'),
-						'drawing_file_name' => $filename,
-						'drawing_created_at' => DB::raw('now()')
-					]);
+		self::addToDB($filename, $ownname);
 		return view('drawfigure');
+	}
+
+	function addToDB($filename, $ownname){
+		DB::table('drawings')->insert([
+			'id' => Auth::user()->id,
+			'drawingname' => $ownname,
+			'drawing_file_name' => $filename,
+			'drawing_created_at' => DB::raw('now()')
+		]);
 	}
 }
