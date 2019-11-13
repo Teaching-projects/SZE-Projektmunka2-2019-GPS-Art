bf6bb0856593acffc524ee567df5c68c2cd94ff4
Mihály Kóródi <mihaly.korodi97@gmail.com>
2019-11-13 09:18:40 +0100

    Removed random things from optimized python code

--- python/tracktopng.py
+++ python/tracktopng.py
@@ -5,5 +5,5 @@ import xml.etree.ElementTree as ET
 ns = '{http://www.topografix.com/GPX/1/1}'
 for trkseg in ET.parse(sys.argv[1]).getroot().iter(ns+"trkseg"):
     plt.axis('off')
-    plt.plot(list(float(trkpt.attrib['lat'])*1.02 for trkpt in trkseg), list(float(trkpt.attrib['lon'])*2 for trkpt in trkseg), linewidth=3, c='black')
+    plt.plot(list(float(trkpt.attrib['lat']) for trkpt in trkseg), list(float(trkpt.attrib['lon']) for trkpt in trkseg), linewidth=3, c='black')
 plt.savefig(sys.argv[2], bbox_inches='tight', dpi=100)
