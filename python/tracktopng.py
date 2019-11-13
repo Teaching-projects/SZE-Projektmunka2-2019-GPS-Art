a7b95dc183511a87b3fd0e2b2897be5eca74aa40
Mihály Kóródi <mihaly.korodi97@gmail.com>
2019-11-13 09:16:48 +0100

    Optimized python code

--- python/tracktopng.py
+++ python/tracktopng.py
@@ -2,19 +2,8 @@ import sys
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 
-root = ET.parse(sys.argv[1]).getroot()
 ns = '{http://www.topografix.com/GPX/1/1}'
-
-def trkseg_to_png(track):
+for trkseg in ET.parse(sys.argv[1]).getroot().iter(ns+"trkseg"):
     plt.axis('off')
-    plt.plot(list(elem['lat'] for elem in track), list(elem['lon'] for elem in track), linewidth=3, c='black')
-
-for trkseg in root.iter(ns+"trkseg"):
-    track = []
-    for trkpt in trkseg:
-        track.append({
-            'lat': float(trkpt.attrib['lat']), 
-            'lon': float(trkpt.attrib['lon'])
-        })
-    trkseg_to_png(track)
+    plt.plot(list(float(trkpt.attrib['lat'])*1.02 for trkpt in trkseg), list(float(trkpt.attrib['lon'])*2 for trkpt in trkseg), linewidth=3, c='black')
 plt.savefig(sys.argv[2], bbox_inches='tight', dpi=100)
