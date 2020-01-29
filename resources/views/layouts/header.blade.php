0e65bd83e2fe22759d806f2131b989c431ea7248
rajfruzsi <34722134+rajfruzsi@users.noreply.github.com>
2020-01-29 19:33:31 +0100

    frontpage design with AngularJS and controller added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,26 +7,28 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<script src="./js.js"></script>
   <link rel="stylesheet" href="http://adampapp.ddns.net/projektmunka/css/style.css" type="text/css">
  </head>
- <body>
+ <body  ng-app="ProjectsCards">
 
 @if(isset(Auth::user()->email))
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
   <div class="carousel-inner">
     <div class="carousel-item active">
-    <img src="https://res.cloudinary.com/im2015/image/upload/w_1000//blog/running_cover_1.jpg" class="d-block w-100" style="height:180px; object-fit:cover;object-position:10% -400px" alt="...">
+    <img src="https://res.cloudinary.com/im2015/image/upload/w_1000//blog/running_cover_1.jpg" class="d-block w-100" style="height:180px; object-fit:cover;object-position:10% -250px" alt="...">
     </div>
     <div class="carousel-item">
-    <img src="https://res.cloudinary.com/im2015/image/upload/w_1000//blog/running_cover_1.jpg" class="d-block w-100" style="height:180px; object-fit:cover;object-position:10% -400px" alt="...">
+    <img src="https://res.cloudinary.com/im2015/image/upload/w_1000//blog/running_cover_1.jpg" class="d-block w-100" style="height:180px; object-fit:cover;object-position:10% -250px" alt="...">
     </div>
     <div class="carousel-item">
-    <img src="https://res.cloudinary.com/im2015/image/upload/w_1000//blog/running_cover_1.jpg" class="d-block w-100" style="height:180px; object-fit:cover;object-position:10% -400px" alt="...">
+    <img src="https://res.cloudinary.com/im2015/image/upload/w_1000//blog/running_cover_1.jpg" class="d-block w-100" style="height:180px; object-fit:cover;object-position:10% -250px" alt="...">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
