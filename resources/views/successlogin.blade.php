9498f6a1a2768092ec5fb93855f8542bc9d6cdd7
rajfruzsi <34722134+rajfruzsi@users.noreply.github.com>
2020-01-30 04:47:53 +0100

    successlogin design changed according to new modifications

--- resources/views/successlogin.blade.php
+++ resources/views/successlogin.blade.php
@@ -12,7 +12,7 @@
 <div>
   <img height="100px" src="http://adampapp.ddns.net/projektmunka/css/placeholder.svg" alt="" style="max-height:100%; max-width:100%;"/>
 </div>
-
+<div class="jumbotron" style="padding-top:10px;">
 <div class="card w3-animate-opacity" style="background-color:rgba(100,100,100,0.1);"> 
 <h2  style="text-align: center">Üdvözöljük, {{ Auth::user()->name}}!</h2>
   <p style="height: 50px"></p>
@@ -21,14 +21,10 @@
   és lehetősége van módosítani saját fiókját, és adatait is.
   <br>Az alábbi kártyák leírással segítik az oldal használatát.</p>
 </div>
-
-
-<div class="card-deck w3-animate-bottom">
-
-    <div ng-controller="AppController">
+<div ng-controller="AppController">
           <div class="card-deck">
             <div ng-repeat="card in cards" class="card" >        
-               <div class="card-body"><h5 class="card-title">@{{card.Name}}</h5>
+               <div class="card-body"><h4 class="card-title">@{{card.Name}}</h4>
                       <p class="card-text">@{{card.Description}}</p>
                     </div>
                       </div>
@@ -41,7 +37,7 @@
 <div ng-controller="AdminController">
           <div class="card-deck">
             <div ng-repeat="card in oldcards" class="card" >        
-               <div class="card-body"><h5 class="card-title">@{{card.Name}}</h5>
+               <div class="card-body"><h4 class="card-title">@{{card.Name}}</h4>
                       <p class="card-text">@{{card.Description}}</p>
                     </div>
                       </div>
@@ -50,4 +46,5 @@
     </div>
     </div>
       @endif
+</div>
 @endsection
